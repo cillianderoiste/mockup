b2abfaef6f5fbe5c09b00e9a1da1def6088e699a
Cillian de Róiste <goibhniu@fsfe.org>
2014-04-02 11:21:22 +0200

    Stashing garbas' nix fixes

--- default.nix
+++ default.nix
@@ -2,7 +2,9 @@
 }:
 let
   pkgs = import <nixpkgs> {};
-  bowerPackages = import ./bower.nix { };
+  bowerComponents = pkgs.callPackage ./bower.nix { 
+    inherit (pkgs) fetchbower buildEnv;
+  };
   nodePackages = import <nixpkgs/pkgs/top-level/node-packages.nix> {
     inherit pkgs;
     inherit (pkgs) stdenv nodejs fetchurl fetchgit;
@@ -10,6 +12,7 @@ let
     self = nodePackages;
     generated = ./package.nix;
   };
+
 in rec {
   tarball = pkgs.runCommand "mockup-1.6.0.tgz" { buildInputs = [ pkgs.nodejs ]; } ''
     mv `HOME=$PWD npm pack ${mockup}` $out
@@ -17,7 +20,7 @@ in rec {
   build = nodePackages.buildNodePackage {
     name = "mockup-1.6.0";
     src = [ tarball ];
-    buildInputs = [ ];
+    buildInputs = [ bowerComponents ];
     deps = with nodePackages; [
       bower
       coveralls
@@ -29,6 +32,7 @@ in rec {
       grunt-contrib-less
       grunt-contrib-requirejs
       grunt-contrib-uglify
+      grunt-contrib-watch
       grunt-jscs-checker
       grunt-karma
       grunt-sed
@@ -42,15 +46,20 @@ in rec {
       karma-sauce-launcher
       karma-script-launcher
       lcov-result-merger
+      less
       mocha
       requirejs
     ];
+    
     postInstall = ''
-      mkdir -p $out/bower_components
-      ${pkgs.lib.concatStrings (map (p: ''
-        ln -s ${pkgs.fetchbower p.name p.version p.target p.outputHash}/packages/*/${p.version} $out/bower_components/${p.name}
-      '') bowerPackages )}
+       ln -s ${bowerComponents} $out/bower_components
     '';
+    # postInstall = ''
+    #   mkdir -p $out/bower_components
+    #   ${pkgs.lib.concatStrings (map (p: ''
+    #     ln -s ${pkgs.fetchbower p.name p.version p.target p.outputHash}/packages/*/${p.version} $out/bower_components/${p.name}
+    #   '') bowerPackages )}
+    # '';
 
     peerDependencies = [];
     passthru.names = [ "mockup" ];
